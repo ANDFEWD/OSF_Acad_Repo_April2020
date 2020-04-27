f5430526427a7ecc1f61eb010135d81d4a9adf6a
ablashch <ablashch@gmail.com>
2020-04-27 22:29:19 +0300

    JS functionallity added v2 and modal added

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,50 @@ $(document).ready(function() {
    
 })
 
+//   $('.nav__item-button').on('mouseover', function(e){
+//     e.preventDefault();  
+//     // console.log($(this));
+//     // console.log(this.parent())
+//     if ($('.nav__item').not($(this).parent()).is('.nav__item--active')){
+//         $('.nav__item').removeClass('nav__item--active');
+       
+//     }
+//     $(this).parent().toggleClass('nav__item--active');
+   
+// })
+
+
+
+//   var navcontanier = $('.nav__submenu-contanier');
+//   var navcontchildren = $('.nav__submenu-contanier').children();
+
+//   navcontanier.on('mouseout', function(e){
+//     e.preventDefault();  
+//     // console.log($(this));
+//     // console.log(this.parent())
+    
+//         $('.nav__item').removeClass('nav__item--active');
+       
+    
+//     // $(this).parent().toggleClass('nav__item--active');
+   
+// })
+
+//  navcontchildren.on('mouseout', function(e){
+//     e.preventDefault();  
+//     // console.log($(this));
+//     // console.log(this.parent())
+    
+//         $('.nav__item').removeClass('nav__item--active');
+       
+    
+//     // $(this).parent().toggleClass('nav__item--active');
+   
+// })
+
+
+
+
 
   $('.navbar-toggler').on('click', function(e){
     e.preventDefault();
@@ -42,8 +86,76 @@ $('.button-heart').on('click', function(e){
 
 })
 
+$('.features__button-currency').on('click', function(e){
+  e.preventDefault();
+  
+  $(this).parent().toggleClass('features__currency--active');
 
 });
+////////////////////////////////////////////////////////////
+// $('.features__button-currency').on('mouseover', function(e){
+//   e.preventDefault();
+  
+//   $(this).parent().toggleClass('features__currency--active');
+
+// });
+//////////////////////////////////////////////////////////////
+// $('.features__button-currency').on('mouseout', function(e){
+//   e.preventDefault();
+  
+//   $(this).parent().removeClass('features__currency--active');
+
+// });
+
+
+
+
+
+$('.features__button-lang').on('click', function(e){
+  e.preventDefault();
+  
+  $(this).parent().toggleClass('features__lang--active');
+
+});
+/////////////////////////////////////////////////////////
+// $('.features__button-lang').on('mouseover', function(e){
+//   e.preventDefault();
+  
+//   $(this).parent().toggleClass('features__lang--active');
+
+// });
+//////////////////////////////////////////////////////////
+// $('.features__button-lang').on('mouseout', function(e){
+//   e.preventDefault();
+  
+//   $(this).parent().removeClass('features__lang--active');
+
+// });
+
+
+// var data = {};
+
+//   console.log(data)
+
+// $('.features__panel-link.user').on('click', function(data){
+
+// $('#modalLoginForm').modal('show')
+// });
+
+$('.features__panel-link.user').on('click', function(data){
+
+   $(this).attr('data-toggle','modal');
+   $(this).attr('data-target','#modalLoginForm');
+
+   });
+
+
+
+
+
+
+});
+
 
 $(document).ready(function () {
 
