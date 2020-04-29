578c003dc6ea08691145eee509c8797714dd6a60
ablashch <ablashch@gmail.com>
2020-04-29 13:26:52 +0300

    preProduction v.1

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
+// var validator = require('validator');
+
+
+
 $(document).ready(function() {
   $('.nav__item-button').on('click', function(e){
     e.preventDefault();  
@@ -8,6 +12,18 @@ $(document).ready(function() {
        
     }
     $(this).parent().toggleClass('nav__item--active');
+   
+})
+
+  var checkWidth = $(window).width();
+
+  $('.nav__submenu-item-button').on('click', function(e){
+    e.preventDefault();  
+    // console.log($(this));
+    // console.log(this.parent())
+    if (checkWidth < 768) { 
+    $(this).parent().toggleClass('nav__submenu-item--active');
+    }
    
 })
 
@@ -22,6 +38,14 @@ $(document).ready(function() {
 //     $(this).parent().toggleClass('nav__item--active');
    
 // })
+$('.description-buttons__top-item').on('click', function(e){
+  e.preventDefault();  
+  // console.log($(this));
+  // console.log(this.parent())
+  
+  $(this).parent().toggleClass('description-buttons__top--active');
+ 
+})
 
 
 
@@ -40,19 +64,16 @@ $(document).ready(function() {
    
 // })
 
-//  navcontchildren.on('mouseout', function(e){
-//     e.preventDefault();  
-//     // console.log($(this));
-//     // console.log(this.parent())
-    
-//         $('.nav__item').removeClass('nav__item--active');
-       
-    
-//     // $(this).parent().toggleClass('nav__item--active');
-   
-// })
+//  
+/////////////////////////////////////
 
 
+
+////////////////////////////////////////
+
+////////////////////////////////////////
+
+////////////////////////////////////////
 
 
 
@@ -86,56 +107,38 @@ $('.button-heart').on('click', function(e){
 
 })
 
-$('.features__button-currency').on('click', function(e){
-  e.preventDefault();
-  
-  $(this).parent().toggleClass('features__currency--active');
 
-});
-////////////////////////////////////////////////////////////
-// $('.features__button-currency').on('mouseover', function(e){
-//   e.preventDefault();
-  
-//   $(this).parent().toggleClass('features__currency--active');
+/////////////Lang and Currency bottons action///////////
 
-// });
-//////////////////////////////////////////////////////////////
-// $('.features__button-currency').on('mouseout', function(e){
-//   e.preventDefault();
-  
-//   $(this).parent().removeClass('features__currency--active');
-
-// });
-
-
-
-
-
-$('.features__button-lang').on('click', function(e){
-  e.preventDefault();
-  
-  $(this).parent().toggleClass('features__lang--active');
-
-});
-/////////////////////////////////////////////////////////
-// $('.features__button-lang').on('mouseover', function(e){
-//   e.preventDefault();
-  
-//   $(this).parent().toggleClass('features__lang--active');
-
-// });
-//////////////////////////////////////////////////////////
-// $('.features__button-lang').on('mouseout', function(e){
-//   e.preventDefault();
-  
-//   $(this).parent().removeClass('features__lang--active');
-
-// });
+     
+       
+        var checkWidth = $(window).width();
+        
+        
+          $('.features__button-currency').on('click', function(e){
+            e.preventDefault();
+            
+            if (checkWidth < 960) { 
+              $(this).parent().toggleClass('features__currency--active');
+          }
+          
+          });
+          
+          $('.features__button-lang').on('click', function(e){
+            e.preventDefault();
+            
+            if (checkWidth < 960) { 
+              $(this).parent().toggleClass('features__lang--active');
+            }
+          });
+        
+      
+            
+    
 
 
-// var data = {};
 
-//   console.log(data)
+/////////////Lang and Currency bottons action///////////
 
 // $('.features__panel-link.user').on('click', function(data){
 
@@ -172,3 +175,240 @@ $(document).ready(function () {
       $('.animated-icon3').toggleClass('open');
     });
   });
+
+  // $("#modalLoginForm").ready(function () {
+  // $("#modalLoginForm").validate({
+  //   rules: {
+  //     email: {
+  //       required: true,
+  //       email: true
+  //     },
+  //     password: {
+  //       required: true,
+  //       minlength: 6,
+  //       pwcheck:true
+  
+  //     }
+  //   },
+  //   messages: {
+  //    email: {
+  //       required: "We need your email address to contact you",
+  //       email: "Your email address must be in the format of name@domain.com"
+  //     },
+  //     password: {
+  //       required: "You need to enter your password here",
+  //       minlength: "Your password should be at least 6 characters length",
+  //       pwcheck: "Your password should contain 1 numeric, 1 uppercase letter, 1 special character"
+  
+  //   } 
+  //  }
+   
+  // //  });
+  
+  // });
+  // $.validator.addMethod("pwcheck", function(value) {
+  //   return /^[A-Z0-9\d=!\-@._*]*$/.test(value) // consists of only these
+  //       // && /[a-z]/.test(value) // has a lowercase letter
+  //       // && /\d/.test(value) // has a digit
+  // })
+  // });
+
+
+
+  // $(document).ready(function () {
+
+    
+   
+      //вешаем плагин на контейнер-картинку
+      //   $(".my-foto-container").imagezoomsl({
+
+      //     descarea: ".big-caption", 				
+		  //     zoomrange: [1.68, 10],
+		  //     zoomstart: 1.68,
+		  //     cursorshadeborder: "10px solid black",
+		  //     magnifiereffectanimate: "fadeIn",	
+      //   });
+	  
+      // //клик по превью-картинке
+      //   $(".my-foto").click(function(){
+
+      //      var that = this;
+
+      //    //копируем атрибуты из превью-картинки в контейнер-картинку
+      //      $(".my-foto-container").fadeOut(600, function(){
+
+      //        $(this).attr("src",               $(that).attr("data-large"))              // путь до small картинки
+      //               .attr("data-large",       $(that).attr("data-large"))       // путь до big картинки
+					
+      //               //дополнительные атрибуты, если есть
+      //               //.attr("data-title",       $(that).attr("data-title"))       // заголовок подсказки
+      //               //.attr("data-help",        $(that).attr("data-help"))        // текст подсказки    
+      //               //.attr("data-text-bottom", $(that).attr("data-text-bottom")) // текст снизу картинки
+                    
+      //               .fadeIn(1000);				
+      //      });
+      //  });	  
+    
+
+    //    $(document).ready(function () {
+    //    function postsCarousel() {
+    //     var checkWidth = $(window).width();
+    //     var owlPost = $(".product-container__galery");
+    //     if (checkWidth > 767) {
+    //       if(typeof owlPost.data('owl.carousel') != 'undefined'){
+    //         owlPost.data('owl.carousel').destroy(); 
+    //       }
+    //       owlPost.removeClass('owl-carousel');
+    //     } else if (checkWidth < 768) {
+    //       owlPost.addClass('owl-carousel');
+    //       owlPost.owlCarousel({
+    //         items : 1,
+    //         slideSpeed : 500,
+    //         animateOut: 'fadeOut',
+    //         touchDrag: false,
+    //         mouseDrag: false,
+    //         autoplay: true,
+    //         autoplaySpeed: 8000,
+    //         autoplayTimeout: 8000,
+    //         dots: true,
+    //         loop: true
+    //       });
+    //     }
+    //   }
+    
+    //   postsCarousel();
+    //   $(window).resize(postsCarousel);   
+    // });   
+// jQuery(function(){
+
+// 	$(".container").imagezoomsl({ 
+		
+// 		descarea: ".big-caption", 				
+// 		zoomrange: [1.68, 10],
+// 		zoomstart: 1.68,
+// 		cursorshadeborder: "10px solid black",
+// 		magnifiereffectanimate: "fadeIn",	
+// 	});
+  
+
+	// $(".tmb-caption img").click(function(){
+
+	//     var that = this;
+	// 	$(".container").fadeOut(600, function(){
+		
+	// 		$(this).attr("src", 	   $(that).attr("src"))
+	// 			   .attr("data-large", $(that).attr("data-tmb-large"))
+	// 			   .fadeIn(1000);				
+	// 	});
+
+	//     return false;
+	// });  
+
+// });
+
+
+
+  // $('.slider').slick({
+  //   slidesToShow: 2,
+  //   slidesToScroll: 1,
+  //   arrows: true,
+  //   // fade: true,
+  //   adaptiveHeight:true,
+  //   // speed:1000,
+  //   // easing:'ease',
+  //   // infinite:true,
+  //   // initialSlide:0,
+  //   // autoplay:false,
+  //   // draggable:true,
+  //   // swipe:true,
+  //   // centerMode: true,
+  //   // focusOnSelect: true,
+  //   vertical:true,
+  //   asNavFor: ".sliderbig"
+    
+  // });
+  // $('.sliderbig').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   fade: true,
+  //   adaptiveHeight:true,
+  //   // speed:1000,
+  //   // easing:'ease',
+  //   // infinite:true,
+  //   // initialSlide:0,
+  //   // autoplay:false,
+  //   // swipe:true,
+  //   vertical:true,
+  //   // centerMode: true,
+  //   // focusOnSelect: true,
+  //   asNavFor: ".slider"
+    
+  // });
+
+
+  // // $('.slider-nav').slick({
+  // //   slidesToShow: 4,
+  // //   slidesToScroll: 1,
+  // //   asNavFor: '.slider',
+  // //   dots: true,
+  // //   centerMode: true,
+  // //   focusOnSelect: true
+  // // });
+  // });
+
+
+
+  $(document).ready(function () {
+  
+  $.validator.addMethod('strongPassword', function(value, element) {
+    // return this.optional(element) 
+    //   || value.length >= 6
+    //   && /\d/.test(value)
+    //   && /[a-z]/i.test(value)
+    //   && /[A-Z]/i.test(value);
+    return /^[A-Za-z0-9\d=!\-@.!#$%^&?_*]*$/.test(value) // consists of only these
+       && /[A-Z]/.test(value) // has a uppercase letter
+       && /[0-9]/.test(value) // has a numeric
+       && /[-@.!#$%^&?_*]/.test(value) // has a symbol
+       && /\d/.test(value); // has a digit
+  }, 'Your password must be at least 6 characters length,contain 1 numeric, 1 uppercase letter, 1 special character.')
+
+  $("#modalLoginForm").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true
+        // remote: "http://localhost:3000/inputValidator"
+      },
+      password: {
+        required: true,
+        minlength:6,
+        strongPassword: true
+      }
+      
+    },
+    messages: {
+      email: {
+        required: 'Please enter an email address.',
+        email: 'Please enter a <em>valid</em> email address.',
+        // remote: $.validator.format("{0} is already associated with an account.")
+      }
+    }
+  });
+  console.log();
+});
+
+
+$(document).ready(function () {
+$('body').on('click', '.password-control', function(){
+	if ($('#password').attr('type') == 'password'){
+		$(this).addClass('view');
+		$('#password').attr('type', 'text');
+	} else {
+		$(this).removeClass('view');
+		$('#password').attr('type', 'password');
+	}
+	return false;
+});
+});
